905bcba3f3b97292979767d96179dd467c7736ef
Christopher Goes <cwgoes@protonmail.ch>
2018-04-23 12:35:09 +0200

    Context-accessible logger field

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -187,9 +187,9 @@ func (app *BaseApp) initFromStore(mainKey sdk.StoreKey) error {
 // NewContext returns a new Context with the correct store, the given header, and nil txBytes.
 func (app *BaseApp) NewContext(isCheckTx bool, header abci.Header) sdk.Context {
 	if isCheckTx {
-		return sdk.NewContext(app.checkState.ms, header, true, nil)
+		return sdk.NewContext(app.checkState.ms, header, true, nil, log.NewNopLogger())
 	}
-	return sdk.NewContext(app.deliverState.ms, header, false, nil)
+	return sdk.NewContext(app.deliverState.ms, header, false, nil, log.NewNopLogger())
 }
 
 type state struct {
@@ -205,7 +205,7 @@ func (app *BaseApp) setCheckState(header abci.Header) {
 	ms := app.cms.CacheMultiStore()
 	app.checkState = &state{
 		ms:  ms,
-		ctx: sdk.NewContext(ms, header, true, nil),
+		ctx: sdk.NewContext(ms, header, true, nil, log.NewNopLogger()),
 	}
 }
 
@@ -213,7 +213,7 @@ func (app *BaseApp) setDeliverState(header abci.Header) {
 	ms := app.cms.CacheMultiStore()
 	app.deliverState = &state{
 		ms:  ms,
-		ctx: sdk.NewContext(ms, header, false, nil),
+		ctx: sdk.NewContext(ms, header, false, nil, log.NewNopLogger()),
 	}
 }
 
